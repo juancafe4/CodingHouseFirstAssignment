5bd0109d18698e77b9ea229c7e2b3d2a1f9ddf0d
Juan Carlos <juancafe2@gmail.com>
2016-07-26 18:09:31 -0700

    Added all the arithmetic operations plus a score system

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  var operation = ["+", "-"]
-  var randOperation = Math.floor(Math.random() * (1- 0 + 1)) + 0;
+  let score = 0;
+  let scoreTag = document.getElementById("score")
+  scoreTag.innerHTML = scoreTag.innerHTML + score
+  let operation = ["+", "-", "*", "/"]
+  let randOperation = Math.floor(Math.random() * (3- 0 + 1)) + 0;
   let firstNum =  Math.floor(Math.random() * (100 - 0 + 1)) + 0;
   let secondNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
   
+  
   let result = 0;
   let str = "";
-  console.log("operation ", operation[randOperation])
+
   if (operation[randOperation] === '+') {
     result = firstNum + secondNum
     str = firstNum + " + " + secondNum + " = " 
@@ -15,6 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
   else if (operation[randOperation] === '-') {
     result = firstNum - secondNum
     str = firstNum + " - " + secondNum + " = " 
+  }
+
+  else if (operation[randOperation] === '*') {
+    result = firstNum * secondNum
+    str = firstNum + " * " + secondNum + " = " 
+  }
+
+  else if (operation[randOperation] === '/') {
+    while (secondNum === 0) {
+      secondNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+    }
+    result = parseFloat(firstNum / secondNum).toFixed(2)
+    str = firstNum + " / " + secondNum + " = " 
   }
   let entrance = str
   let main = document.getElementById("main")
@@ -26,8 +43,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", (event) => {
+
+
       let target = event.target.textContent
-      if (target !== "Clear" && target !== "Submit") {
+      if (target !== "Clear" && target !== "Submit"
+        && target !== "Skip") {
         str += target
         answer += target
         main.innerHTML = str;
@@ -42,22 +62,62 @@ document.addEventListener("DOMContentLoaded", () => {
       else if (target === "Submit") {
         if (result === parseInt(answer)) {
           alert("Correct answer!")
+          scoreTag.innerHTML = "Score: " + ++score;
+        }
+        else if (operation[randOperation] === '/' &&  result == parseFloat(answer)) {
+          alert("Correct answer!")
+          scoreTag.innerHTML = "Score: " + ++score;
         }
         else {
           alert("Incorrect answer!")
           main.innerHTML = entrance + result
+          scoreTag.innerHTML = "Score: " + --score;
         }
         
+        reset();
+      }
+
+      else if (target === "Skip") {
+        main.innerHTML = entrance + result
+        reset();
+      }
+    });
+  }
+
+  /*document.addEventListener('keydown', function(event) {
+    console.log("key ", String.fromCharCode(event.keyCode))
+    if ((event.keyCode >= 48 && event.keyCode <= 57)
+      || event.keyCode == 189 || event.keyCode === 190)  {
+      alert("key pressed " + event.keyCode)
+    }
+  });*/
+  function reset() {
         setTimeout(function() {
+          randOperation = Math.floor(Math.random() * (3- 0 + 1)) + 0;
           firstNum =  Math.floor(Math.random() * (100 - 0 + 1)) + 0;
           secondNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
-          result = firstNum + secondNum
-          str = firstNum + " + " + secondNum + " = " 
+            if (operation[randOperation] === '+') {
+              result = firstNum + secondNum
+              str = firstNum + " + " + secondNum + " = " 
+            }
+            else if (operation[randOperation] === '-') {
+              result = firstNum - secondNum
+              str = firstNum + " - " + secondNum + " = " 
+            }
+
+            else if (operation[randOperation] === '*') {
+              result = firstNum * secondNum
+              str = firstNum + " * " + secondNum + " = " 
+            }
+
+            else if (operation[randOperation] === '/') {
+              result = parseFloat(firstNum / secondNum).toFixed(2)
+              str = firstNum + " / " + secondNum + " = " 
+            }
           entrance = str
           answer = ""
           main.innerHTML = str;
         }, 2500)
-      }
-    });
   }
 });
+
